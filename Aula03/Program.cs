d62d38770e4a606ab6ee6da0b85afb78805657e4
Livaylouco1 <lbambam.2000@gmail.com>
2025-02-19 13:48:58 +0000

    Teste de televisao

---
+++ Aula03/Program.cs
@@ -0,0 +1,11 @@
+﻿using Aula03;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Televisao tvSala = new Televisao(55);
+
+Console.WriteLine($"O tamanho da v é: {tvSala.Tamanho}");
+
+
+
